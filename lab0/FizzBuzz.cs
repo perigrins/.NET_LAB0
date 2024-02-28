fa2c364664d8f89e17460c14cfbf66a2b686d8d7
hania <hania@HANIA-LAPTOP>
2024-02-28 19:26:58 +0100

    Dodaj pliki projektów.

---
+++ lab0/FizzBuzz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab0
+{
+	internal class FizzBuzz
+	{
+		private int uppB = 10;
+		public FizzBuzz(int a)
+		{
+			uppB = a;
+		}
+
+		public void Display()
+		{
+			for(int i=1; i<=uppB; i++)
+			{
+				if (i % 3 == 0 && i % 5 == 0)
+				{
+					Console.WriteLine("FizzBuzz\n");
+				}
+				else if (i % 3 == 0)
+				{
+					Console.WriteLine("Fizz\n");
+				}
+				else if (i % 5 == 0)
+				{
+					Console.WriteLine("Buzz\n");
+				}
+				else Console.WriteLine(i + "\n");
+			}
+		}
+	}
+}
---
+++ lab0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab0
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			FizzBuzz fz1 = new FizzBuzz(15);
+			fz1.Display();
+		}
+	}
+}
